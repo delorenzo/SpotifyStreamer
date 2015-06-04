b8334c269f1d17057716ba442dd15dfc7b871303
Julie De Lorenzo <delorenzo.julie@gmail.com>
2015-06-03 20:26:49 -0400

    Initial commit

---
+++ app/src/main/java/com/julie/spotifystreamer/MainActivity.java
@@ -0,0 +1,48 @@
+package com.julie.spotifystreamer;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+
+
+public class MainActivity extends ActionBarActivity {
+    SpotifyService spotify;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SpotifyApi mSpotifyApi = new SpotifyApi();
+        spotify = mSpotifyApi.getService();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void searchForArtist(String artist)
+    {
+        spotify.getArtist(artist);
+    }
+}

3dcd567c76c83917300699f108482042488542e2
Julie De Lorenzo <delorenzo.julie@gmail.com>
2015-07-26 23:43:40 -0400

    added drawable icons, update mediaplayerservice

--- app/src/main/java/com/julie/spotifystreamer/MediaPlayerService.java
+++ app/src/main/java/com/julie/spotifystreamer/MediaPlayerService.java
@@ -1,8 +1,10 @@
 package com.julie.spotifystreamer;
 
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
@@ -11,6 +13,7 @@ import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.os.IBinder;
 import android.os.PowerManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import java.io.IOException;
@@ -22,44 +25,83 @@ import java.io.IOException;
  * See https://developer.android.com/guide/topics/media/mediaplayer.html for details.
  */
 public class MediaPlayerService extends Service implements MediaPlayer.OnPreparedListener,
-        MediaPlayer.OnErrorListener, MediaPlayer.OnCompletionListener{
+        MediaPlayer.OnErrorListener, MediaPlayer.OnCompletionListener,
+        AudioManager.OnAudioFocusChangeListener{
+
     private static final String ACTION_PLAY = "com.julie.spotifystreamer.action.PLAY";
     private static final String LOG_TAG = MediaPlayerService.class.getSimpleName();
     private static final String WIFI_LOCK_TAG = "mediaPlayerWifiLock";
     public static final String ARG_SONG_NAME = "songName";
     public static final String ARG_URI = "uri";
+    private static final int mNotificationId = 1;
 
     MediaPlayer mMediaPlayer = null;
     private String mUriString;
     private String mSongName;
     private WifiManager.WifiLock mWifiLock;
 
+    @Override
+    public void onAudioFocusChange(int focusChange) {
+        switch (focusChange) {
+            case AudioManager.AUDIOFOCUS_GAIN:
+                if (mMediaPlayer == null) {
+                    initMediaPlayer();
+                }
+                else if (!mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.start();
+                }
+                mMediaPlayer.setVolume(1.0f, 1.0f);
+                break;
+            case AudioManager.AUDIOFOCUS_LOSS:
+                break;
+            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                break;
+            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+                break;
+        }
+    }
+
     public int onStartCommand(Intent intent, int flags, int startId) {
         if (intent.getAction().equals(ACTION_PLAY)) {
             mUriString = intent.getStringExtra(ARG_URI);
             mSongName = intent.getStringExtra(ARG_SONG_NAME);
-            mMediaPlayer = new MediaPlayer();
-            setupMediaPlayer();
-            createNotification();
+
+            AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+            int result = audioManager.requestAudioFocus(this, AudioManager.STREAM_MUSIC,
+                    AudioManager.AUDIOFOCUS_GAIN);
+            if (result != AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                Log.e(LOG_TAG, "Audio focus request was denied.  Media player will not be started.");
+                return -1;
+            }
+
+            initMediaPlayer();
+            startNotification();
         }
         return 0;
     }
 
     //make the service a foreground service by creating a notification for the status bar.
-    private void createNotification() {
-//        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0,
-//                new Intent(getApplicationContext(), TrackPlayerActivity.class),
-//                PendingIntent.FLAG_UPDATE_CURRENT);
-//        Notification notification = new Notification.Builder(getApplicationContext())
-//                .setContentTitle("Now playing:  " + mSongName)
-//                .setSmallIcon(R.mipmap.ic_launcher)
-//                .build();
-//        notification.tickerText = getString(R.string.app_name);
-//        //TODO:  use the material icon for play
-//        notification.icon = R.drawable.abc_ic_menu_copy_mtrl_am_alpha;
-//        notification.flags |= Notification.FLAG_ONGOING_EVENT;
-//        notification.setLatestEventInfo(getApplicationContext(), getString(R.string.app_name), "Playing:  " + mSongName, pendingIntent);
-//        startForeground(notification);
+    private void startNotification() {
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0,
+                new Intent(getApplicationContext(), TrackPlayerActivity.class),
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(getApplicationContext())
+                .setContentTitle("Now playing:  ")
+                .setSmallIcon(R.drawable.ic_queue_music_black_24dp)
+                .setContentText(mSongName)
+                .setContentIntent(pendingIntent);
+        Notification notification = mBuilder.build();
+
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(mNotificationId, notification);
+
+        startForeground(mNotificationId, notification);
+    }
+
+    private void updateNotificationWithText(String msg) {
+
     }
 
     @Override
@@ -67,7 +109,8 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnPrepare
 
     }
 
-    private void setupMediaPlayer() {
+    private void initMediaPlayer() {
+        mMediaPlayer = new MediaPlayer();
         setListeners();
         mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         mMediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
@@ -139,7 +182,7 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnPrepare
                 break;
         }
         mp.reset();
-        setupMediaPlayer();
+        initMediaPlayer();
         return true;
     }
 }
